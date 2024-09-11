733b6d92971566b70fc8ac43b8499bbd95834c2a
kmoyse <kevin.moyse@apizee.com>
2024-09-11 14:29:43 +0200

    tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { DecimalPipe, PercentPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import localeFr from '@angular/common/locales/fr';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeFr);
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DecimalPipe, PercentPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
